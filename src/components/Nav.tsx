63b2ffce254277a690dc06a5caccfd2fa570fe28
ZherdevMak <Zherdev.mak@gmail.com>
2022-05-21 13:10:05 +0300

    lesson14

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import clAss from './Nav.module.css'
+
+
 
 function NavBar() {
-    return (<nav className='nav'>
-        <li>
+    return (<nav className={clAss.nav}>
+        <li className={clAss.item}>
             <a>Profile</a>
         </li>
-        <li>
+        <li className={`${clAss.item} ${clAss.active}`}>
             <a>Messages</a>
         </li>
-        <li>
+        <li className={clAss.item}>
             <a>News</a>
         </li>
-        <li>
+        <li className={clAss.item}>
             <a>Music</a>
         </li>
-        <li>
+        <li className={clAss.item}>
             <a>Settings</a>
         </li>
     </nav>)
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,22 +1,24 @@
 import React from "react";
+import comp from './Profile.module.css'
+
 
 function Profile() {
-    return (<main className='content'>
+    return (<main className={comp.content}>
         <img src='https://html5css.ru/css/img_forest.jpg'></img>
         <article>
             ava+discription
         </article>
-        <article>
+        <article className='posts'>
             My posts
             <article>
                 New post
             </article>
         </article>
         <article>
-            <article>
+            <article className={comp.item}>
                 Post 1
             </article>
-            <article>
+            <article className={comp.item}>
                 Post 2
             </article>
         </article>
