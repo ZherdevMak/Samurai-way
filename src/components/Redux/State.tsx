110609de589257ff0621b5d64692056be725788c
ZherdevMak <Zherdev.mak@gmail.com>
2022-06-28 01:06:23 +0300

    49 done

--- src/components/Redux/State.tsx
+++ src/components/Redux/State.tsx
@@ -42,84 +42,84 @@ export type StoreType = {
     subscribe: (observer: () => void) => void
     dispatch: (action: any) => void
 }
-export type ActionTypes = AddPostType | AddNewText | AddMessageType | AddMessageTextType
-export type AddPostType = {
-    type: 'ADD-POST'
-}
-export type AddNewText = {
-    type: 'ADD-NEW-TEXT'
-    newText: string
-}
-export type AddMessageType = {
-    type: 'ADD-MESSAGE'
-}
-export type AddMessageTextType = {
-    type: 'ADD-NEW-MESSAGE-TEXT'
-    newMessageValue:string
-}
-
-export let store: StoreType = {
-    _state: {
-        dialogs: {
-            dialogs: [
-                {name: 'Dimych', id: 1},
-                {name: 'Andrey', id: 2},
-                {name: 'Sveta', id: 3},
-                {name: 'Valera', id: 4},
-                {name: 'Sasha', id: 5},
-            ],
-            messages: [
-                {message: 'Hi', id: v1()},
-                {message: 'Hello', id: v1()},
-                {message: 'Yo', id: v1()},
-                {message: 'How are you?', id: v1()},
-            ],
-            newMessageValue: ""
-        },
-        profile: {
-            posts: [
-                {id: 1, message: 'Hi, how are you?', likesCount: 12},
-                {id: 2, message: 'It\'s my first post', likesCount: 10},
-                {id: 3, message: 'Hi, how are you?', likesCount: 2},
-                {id: 4, message: 'Hi, how a?', likesCount: 2},
-            ],
-            newPostValue: ""
-        },
-        sidebar: [
-            {
-                id: 1,
-                name: 'Sasha',
-                avatar: 'https://www.animeoutline.com/wp-content/uploads/2018/06/anime_cat_drawing.png'
-            },
-            {
-                id: 2,
-                name: 'Masha',
-                avatar: 'https://www.animeoutline.com/wp-content/uploads/2018/06/anime_cat_drawing.png'
-            },
-            {
-                id: 3,
-                name: 'Luba',
-                avatar: 'https://www.animeoutline.com/wp-content/uploads/2018/06/anime_cat_drawing.png'
-            },
-        ]
-    },
-    getState() {
-        return this._state
-    },
-    renderEntireTree() {
-    },
-    subscribe(observer) {
-        this.renderEntireTree = observer
-    },
-
-    dispatch(action) {
-        // @ts-ignore
-        this._state.profile = ProfileReduser(action, this._state.profile)
-        // @ts-ignore
-        this._state.dialogs = DialogsReduser(action, this._state.dialogs)
-        this._state.sidebar = SidebarReduser(action, this._state.sidebar)
-        this.renderEntireTree()
-    }
-}
-
-
+// export type ActionTypes = AddPostType | AddNewText | AddMessageType | AddMessageTextType
+// export type AddPostType = {
+//     type: 'ADD-POST'
+// }
+// export type AddNewText = {
+//     type: 'ADD-NEW-TEXT'
+//     newText: string
+// }
+// export type AddMessageType = {
+//     type: 'ADD-MESSAGE'
+// }
+// export type AddMessageTextType = {
+//     type: 'ADD-NEW-MESSAGE-TEXT'
+//     newMessageValue:string
+// }
+//
+// export let store: StoreType = {
+//     _state: {
+//         dialogs: {
+//             dialogs: [
+//                 {name: 'Dimych', id: 1},
+//                 {name: 'Andrey', id: 2},
+//                 {name: 'Sveta', id: 3},
+//                 {name: 'Valera', id: 4},
+//                 {name: 'Sasha', id: 5},
+//             ],
+//             messages: [
+//                 {message: 'Hi', id: v1()},
+//                 {message: 'Hello', id: v1()},
+//                 {message: 'Yo', id: v1()},
+//                 {message: 'How are you?', id: v1()},
+//             ],
+//             newMessageValue: ""
+//         },
+//         profile: {
+//             posts: [
+//                 {id: 1, message: 'Hi, how are you?', likesCount: 12},
+//                 {id: 2, message: 'It\'s my first post', likesCount: 10},
+//                 {id: 3, message: 'Hi, how are you?', likesCount: 2},
+//                 {id: 4, message: 'Hi, how a?', likesCount: 2},
+//             ],
+//             newPostValue: ""
+//         },
+//         sidebar: [
+//             {
+//                 id: 1,
+//                 name: 'Sasha',
+//                 avatar: 'https://www.animeoutline.com/wp-content/uploads/2018/06/anime_cat_drawing.png'
+//             },
+//             {
+//                 id: 2,
+//                 name: 'Masha',
+//                 avatar: 'https://www.animeoutline.com/wp-content/uploads/2018/06/anime_cat_drawing.png'
+//             },
+//             {
+//                 id: 3,
+//                 name: 'Luba',
+//                 avatar: 'https://www.animeoutline.com/wp-content/uploads/2018/06/anime_cat_drawing.png'
+//             },
+//         ]
+//     },
+//     getState() {
+//         return this._state
+//     },
+//     renderEntireTree() {
+//     },
+//     subscribe(observer) {
+//         this.renderEntireTree = observer
+//     },
+//
+//     dispatch(action) {
+//         // @ts-ignore
+//         this._state.profile = ProfileReduser(action, this._state.profile)
+//         // @ts-ignore
+//         this._state.dialogs = DialogsReduser(action, this._state.dialogs)
+//         this._state.sidebar = SidebarReduser(action, this._state.sidebar)
+//         this.renderEntireTree()
+//     }
+// }
+//
+//
--- src/components/Redux/StoreContext.tsx
+++ src/components/Redux/StoreContext.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
-const StoreContext = React.createContext(null)
-export const Provider = (props:any) => {
-    return
-
-};
-
-export default StoreContext;
+//
+// const StoreContext = React.createContext(null)
+// export const Provider = (props:any) => {
+//     return
+//
+// };
+//
+// export default StoreContext;
