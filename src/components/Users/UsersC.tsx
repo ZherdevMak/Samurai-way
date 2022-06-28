f02679e75b646de109599435c0611d0deb61b788
ZherdevMak <Zherdev.mak@gmail.com>
2022-06-28 23:42:50 +0300

    54 life cycle

--- src/components/Users/UsersC.tsx
+++ src/components/Users/UsersC.tsx
@@ -7,8 +7,8 @@ import {UsersPropsType} from "./UsersContainer";
 
 
 class Users extends React.Component<UsersPropsType> {
-    constructor(props:UsersPropsType) {
-        super(props);
+
+        componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
     })
