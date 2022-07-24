6374b4fb16d30bf728926e1062e4b7c0536718aa
ZherdevMak <Zherdev.mak@gmail.com>
2022-07-24 14:03:38 +0300

    75 begin of login form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
+type FormDataType = {
+    Login:string
+    Password:string
+    checkbox:boolean
+}
 const Login = () => {
+    const onSubmitFinction = (formData:FormDataType) => {
+    }
     return (
         <div>
             <h2>Login</h2>
+            <LoginReduxForm onSubmit={onSubmitFinction}/>
         </div>
     );
 };
+export default Login
 
-export default Login;
+export const LoginForm :React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'Login'} component={'input'}/>
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'Password'} component={'input'}/>
+            </div>
+            <div>
+                <Field type={'checkbox'} name={'checkbox'} component={'input'}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+
+    );
+};
+
+const LoginReduxForm = reduxForm<FormDataType>({
+        form: 'login'
+    }
+)(LoginForm)
--- src/components/Redux/ReduxStore.ts
+++ src/components/Redux/ReduxStore.ts
@@ -5,6 +5,8 @@ import SidebarReduser from "./SidebarReduser";
 import UsersReduser from "./UsersReduser";
 import {AuthReduser} from "./Auth-reducer";
 import thunkMiddleware from "redux-thunk"
+import { reducer as formReducer } from 'redux-form'
+
 
 export type stateType = ReturnType<typeof store.getState>
 let redusers = combineReducers({
@@ -12,7 +14,8 @@ let redusers = combineReducers({
     dialogs: DialogsReduser,
     sidebar: SidebarReduser,
     users: UsersReduser,
-    auth: AuthReduser
+    auth: AuthReduser,
+    form:formReducer
 })
 let store = legacy_createStore(redusers, applyMiddleware(thunkMiddleware))
 // @ts-ignore
